880eafc7b99e6e871972e8a426939b889dab2bf1
SametKisi <samet12kisi@gmail.com>
2026-08-31 12:01:45 +0300

    feat: supabase client js değişti

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY // anon key yerine
+);
