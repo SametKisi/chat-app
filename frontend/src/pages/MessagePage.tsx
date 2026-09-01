0f33bea961b6b3ef3c48bba1dbbd520f4fc62d87
SametKisi <samet12kisi@gmail.com>
2026-09-01 10:30:04 +0300

    presence.js dosya adı düzeltildi

--- frontend/src/pages/MessagePage.tsx
+++ frontend/src/pages/MessagePage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { PaperPlaneRightIcon, UserIcon, UsersThree, ChatsTeardrop, Prohibit, ArrowLeft } from "@phosphor-icons/react";
 import { useChatStore } from "../store/useChatStore.ts";
 import { supabase } from '../../supabaseClient';
+import { usePresence } from "../hooks/usePrefesences.ts";
 
 const MessagePage = () => {
     const { 
@@ -16,6 +17,8 @@ const MessagePage = () => {
         receiveIncomingMessage,
         setActiveChat
     } = useChatStore() as any;
+
+    usePresence(currentUser?.id);
 
     const [newMessage, setNewMessage] = useState("");
     const scrollRef = useRef<HTMLDivElement>(null);
